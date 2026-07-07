59294a1b0b73fdf62466ddd8653223df4eef6dfe
Your Name <ausstrainsnetwork@gmail.com>
2026-07-07 18:45:42 +1000

    fix: restore corrupted achievement files (plain-text prepended to source)

--- dfc_frontend/dfc_app/lib/achievement_repository.dart
+++ dfc_frontend/dfc_app/lib/achievement_repository.dart
@@ -1,4 +1,4 @@
-ck list and tick off completetion heres your next list word for word dot for dot no errorsimport '../../api_service.dart';
+import '../../api_service.dart';
 import '../models/achievement_model.dart';
 
 class AchievementRepository {
